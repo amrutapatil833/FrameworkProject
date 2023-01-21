655994ae814ffcc05c5d823733f0cffd071d7a56
amrutapatil833 <amrutapatil833@gmail.com>
2023-01-21 21:48:52 +0530

    changes in TestBase class

--- src/main/java/Base/TestBase.java
+++ src/main/java/Base/TestBase.java
@@ -15,7 +15,9 @@ public class TestBase {
 		WebDriverManager.chromedriver().setup();
 	    driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		driver.get(ReadData.readPropertyFile("url"));
 		//driver.get("https://www.saucedemo.com/");
 	}
+     
 }
